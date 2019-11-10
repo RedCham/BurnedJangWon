0eddb8d157b4cf61198fcc7d7797a36516074092
RedCham <gus4907@naver.com>
2019-11-10 21:38:27 +0900

    first commit

---
+++ app/src/main/java/com/example/burnedjangwon/SignUpActivity.java
@@ -0,0 +1,95 @@
+package com.example.burnedjangwon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import static com.example.burnedjangwon.Util.showToast;
+
+public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        findViewById(R.id.signUpButton).setOnClickListener(onClickListener);
+        findViewById(R.id.gotoLoginButton).setOnClickListener(onClickListener);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.signUpButton:
+                    signUp();
+                    break;
+                case R.id.gotoLoginButton:
+                    myStartActivity(LoginActivity.class);
+                    break;
+            }
+        }
+    };
+
+    private void signUp() {
+        String email = ((EditText)findViewById(R.id.emailEditText)).getText().toString();
+        String password = ((EditText)findViewById(R.id.passwordEditText)).getText().toString();
+        String passwordCheck = ((EditText)findViewById(R.id.passwordCheckEditText)).getText().toString();
+
+        if(email.length() > 0 && password.length() > 0 && passwordCheck.length() > 0){
+            if(password.equals(passwordCheck)){
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    showToast(SignUpActivity.this, "회원가입에 성공하였습니다.");
+                                    //myStartActivity(MainActivity.class);
+                                } else {
+                                    if(task.getException() != null){
+                                        showToast(SignUpActivity.this, task.getException().toString());
+                                    }
+                                }
+                            }
+                        });
+            }else{
+                showToast(SignUpActivity.this, "비밀번호가 일치하지 않습니다.");
+            }
+        }else {
+            showToast(SignUpActivity.this, "이메일 또는 비밀번호를 입력해 주세요.");
+        }
+    }
+
+    private void myStartActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+}
